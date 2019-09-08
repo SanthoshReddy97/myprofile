2d1c55bc5c6db987336bfb6313448b73cd9c4d83
SanthoshReddy97 <santhosh.eedulapalli97@gmail.com>
2019-09-09 01:25:52 +0530

    updated

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path('home', views.home, name="home")
+    path('', views.home, name="home"),
+    path('home', views.home, name="home"),
+    path('getall', views.getall, name="getall")
 ]
--- myprofile/views.py
+++ myprofile/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.contrib import messages
+from django.core import serializers
 from .models import user
+import hashlib
 # Create your views here.
 def home(request):
     if request.method == "POST":
@@ -8,13 +11,22 @@ def home(request):
         email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-
-        newUser = user(username= username, email= email, password= password1)
-        newUser.save()
-        return HttpResponse("deltails stored")
-
+        if (password1 == password2):  
+            password1 = hashlib.sha256(str.encode(password1)).hexdigest()
+            newUser = user(username= username, email= email, password= password1)
+            newUser.save()
+            return HttpResponse("deltails stored")
+        else:
+            messages.info(request, "password didnot match")
+            return redirect("home")
     else:
         return render(request, 'index.html')
+def getall(request):
+    data = serializers.serialize("json", user.objects.all(), fields=('username', 'email'))
+    return HttpResponse(data)
+
+def newhome(request):
+    return HttpResponse("Hello world")
     
 
     
